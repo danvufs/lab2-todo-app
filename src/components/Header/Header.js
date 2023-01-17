40510861ab9573ff319c1e2248298e44f944e4e6
danvu <d_vu@fanshaweonline.ca>
2023-01-17 10:41:19 -0500

    add the styled components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,55 @@
-// Purpose: Header component
+/* Version 2 */
+
+import styled from 'styled-components';
+
+// creating a styled component for the header container
+const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #3498db;
+  color: white;
+  padding: 20px;
+`;
+
+// creating a styled component for the title
+const Title = styled.div`
+  font-size: 2em;
+  font-weight: bold;
+`;
+
+// creating a styled component for the author
+const Author = styled.div`
+  font-size: 1.5em;
+  font-style: italic;
+`;
+
 function Header() {
-  // return the JSX for the header
   return (
-    <header>
+    // using the header container styled component
+    <HeaderContainer>
       {/* Todo App title */}
-      <div>Todo App</div>
+      <Title>Todo App</Title>
       {/* Author name */}
-      <div>by Dan Vu</div>
-    </header>
+      <Author>by Dan Vu</Author>
+    </HeaderContainer>
   );
 }
 
 export default Header;
+
+/* Version 1 */
+// // Purpose: Header component
+// function Header() {
+//   // return the JSX for the header
+//   return (
+//     <header>
+//       {/* Todo App title */}
+//       <div>Todo App</div>
+//       {/* Author name */}
+//       <div>by Dan Vu</div>
+//     </header>
+//   );
+// }
+
+// export default Header;
--- src/components/Tasks/Tasks.js
+++ src/components/Tasks/Tasks.js
@@ -1,6 +1,45 @@
+/* Version 2 */
 import { useState } from 'react';
 import uuid from 'react-uuid';
 import Task from './Task/Task';
+import styled from 'styled-components';
+
+// creating a styled component for the tasks wrapper, TasksTitle, and ClearButton
+const TasksWrapper = styled.div`
+    position: relative;
+    text-align: center;
+    width: 100%;
+    margin: 0 auto;
+    padding-top: 50px;
+    background-color: #f1f1f1;
+    border-radius: 10px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+`;
+
+const TasksTitle = styled.h2`
+    position: absolute;
+    top: 0;
+    z-index: 1;
+    font-size: 2em;
+    font-weight: bold;
+    margin: 0;
+    padding: 10px;
+    align-self: flex-start;
+`;
+
+const ClearButton = styled.button`
+    background-color: #e74c3c;
+    color: white;
+    padding: 10px 20px;
+    border-radius: 5px;
+    margin-top: 20px;
+    font-size: 1em;
+    align-self: center;
+`;
+
 
 // Tasks component that renders a list of tasks and their associated buttons
 function Tasks() {
@@ -32,18 +71,67 @@ function Tasks() {
         const filteredTasks = tasks.filter((task) => task.id !== id);
         setTasks(filteredTasks);
     }
-
+    // Return the JSX for the Tasks component
     return (
-        <>
-            <h2>These are the tasks:</h2>
-            {/* map over the tasks array and render a Task component for each */}
+        <TasksWrapper>
+            <TasksTitle>These are the tasks:</TasksTitle>
             {tasks.map((task, index) =>
             (<Task key={index} task={task} onStatusChange={handleStatusChange}
                 onTaskRemove={handleTaskRemove} />))}
-            <hr />
-            <button onClick={handleClearTasks}>Clear Tasks</button>
-        </>
+            <ClearButton onClick={handleClearTasks}>Clear Tasks</ClearButton>
+        </TasksWrapper>
     );
 }
 
 export default Tasks;
+
+/* Version 1 */
+// import { useState } from 'react';
+// import uuid from 'react-uuid';
+// import Task from './Task/Task';
+
+// // Tasks component that renders a list of tasks and their associated buttons
+// function Tasks() {
+//     // useState hook to manage the tasks state
+//     const [tasks, setTasks] = useState([
+//         { id: uuid(), description: 'Walk the dog', done: true },
+//         { id: uuid(), description: 'Wash the car', done: false },
+//         { id: uuid(), description: 'Finish the lab', done: false }
+//     ]);
+
+//     // callback function to handle when the "Clear Tasks" button is clicked
+//     const handleClearTasks = () => {
+//         setTasks([]);
+//     }
+
+//     // callback function to handle when the "Change Status" button is clicked
+//     const handleStatusChange = (id) => {
+//         // create a copy of the tasks array
+//         const updatedTasks = [...tasks];
+//         // update the task's status in the copied array
+//         updatedTasks.forEach((task) => { if (task.id === id) { task.done = !task.done; } });
+//         // set the tasks state to the updated copied array
+//         setTasks(updatedTasks);
+//     }
+
+//     // callback function to handle when the "Remove" button is clicked
+//     const handleTaskRemove = (id) => {
+//         // filter out the task to be removed and set the tasks state to the filtered array
+//         const filteredTasks = tasks.filter((task) => task.id !== id);
+//         setTasks(filteredTasks);
+//     }
+
+//     return (
+//         <>
+//             <h2>These are the tasks:</h2>
+//             {/* map over the tasks array and render a Task component for each */}
+//             {tasks.map((task, index) =>
+//             (<Task key={index} task={task} onStatusChange={handleStatusChange}
+//                 onTaskRemove={handleTaskRemove} />))}
+//             <hr />
+//             <button onClick={handleClearTasks}>Clear Tasks</button>
+//         </>
+//     );
+// }
+
+// export default Tasks;
